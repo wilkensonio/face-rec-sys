b76775309f638a22b80cfb0d0958d184823266e9
Wilkenson <wilkensoncode@gmail.com>
2024-06-23 08:58:19 -0400

    create roc report and roc plt

--- model_trainer.py
+++ model_trainer.py
@@ -17,9 +17,10 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_curve, auc
 from data_processor import DataProcessor
 from report_manager import ReportManager
+from sklearn.preprocessing import LabelBinarizer
 
 
 class ModelTrainer(DataProcessor, ReportManager):
@@ -98,7 +99,20 @@ class ModelTrainer(DataProcessor, ReportManager):
         return report, testing_labels, predictions, 'Decision Tree'
 
     def evaluation_metrics(self, testing_labels, predictions,
-                           model_name, output_dir='confusion_matrices_plt') -> None:
+                           model_name, output_dir='confusion_matrices_plt') -> tuple[float, np.ndarray, str]:
+        """
+        Plots a confusion matrix for a multiclass classification problem.
+
+        Args:
+            testing_labels (List): True labels of the testing set.
+            predictions (List): Predicted labels for the testing set.
+            model_name (str): Name of the model for which the confusion matrix is being plotted.
+            output_dir (str): Directory to save the confusion matrix plot. Default is 'confusion_matrices'.
+
+        Returns:
+            tuple: A tuple containing the accuracy score, confusion matrix, and model name.
+        """
+
         accuracy = accuracy_score(
             testing_labels, predictions)
         conf_matrix = confusion_matrix(testing_labels, predictions)
@@ -116,3 +130,55 @@ class ModelTrainer(DataProcessor, ReportManager):
         plt.close()
 
         return accuracy, conf_matrix, model_name
+
+    def roc_curve(self, testing_labels, predictions,
+                  model_name, output_dir='roc_curves_plt'
+                  ) -> tuple[dict, dict, dict]:
+        """
+        Plots One-vs-Rest (OvR) ROC curves for a multiclass classification problem.
+
+        Args:
+            testing_labels (List): True labels of the testing set.
+            predictions (List): Predicted probabilities or decision function scores for the testing set.
+            model_name (str): Name of the model for which the ROC curves are being plotted.
+            output_dir (str): Directory to save the ROC curve plot. Default is 'roc_curves'.
+
+        Returns:
+            tuple: A tuple containing the false positive rate, true positive rate, and ROC area.
+        """
+        os.makedirs(output_dir, exist_ok=True)
+
+        lb = LabelBinarizer()
+        lb.fit(testing_labels)
+        y_true = lb.transform(testing_labels)
+
+        if predictions.ndim == 1:
+            predictions = predictions.reshape(-1, 1)
+
+        # Compute ROC curve and ROC area for each class
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+
+        for i in range(len(lb.classes_)):
+            fpr[i], tpr[i], _ = roc_curve(y_true[:, i], predictions[:, 0])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+
+        #  plot the roc curve for the model and save it to a file in the output directory
+        plt.figure()
+        colors = ['blue', 'red', 'green', 'purple',
+                  'orange', 'brown', 'pink', 'gray', 'olive', 'cyan', 'magenta', 'yellow', 'black']
+        for i, color in zip(range(len(lb.classes_)), colors):
+            plt.plot(fpr[i], tpr[i], color=color, lw=2,
+                     label=f'ROC curve (class {lb.classes_[i]}) (area = {roc_auc[i]:0.2f})')
+
+        plt.plot([0, 1], [0, 1], color='gray', lw=1, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title(f'ROC Curves - {model_name}')
+        plt.legend(loc="lower right")
+        plt.savefig(os.path.join(output_dir, f'ROC_{model_name}.png'))
+        plt.close()
+        return fpr, tpr, roc_auc
--- report_manager.py
+++ report_manager.py
@@ -1,8 +1,9 @@
 import os
 import numpy as np
+import os
+import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score, confusion_matrix
-
+from sklearn.metrics import roc_curve, auc
 """
 Report Manager for Face Recognition
 This class is responsible for generating and writing the 
@@ -12,7 +13,9 @@ And also for plotting the confusion matrix.
 
 
 class ReportManager:
-    def write_report(self, report, accuracy, conf_matrix, model_name, output_dir='classification_reports'):
+    def classification_report(self, report, accuracy, conf_matrix, model_name, type='',
+                              output_dir='classification_reports'
+                              ) -> None:
         report_file = os.path.join(output_dir, f'report_{model_name}.txt')
         with open(report_file, 'w') as f:
             f.write(f"Model: {model_name}\n")
@@ -21,6 +24,17 @@ class ReportManager:
             f.write(f"{model_name} Confusion Matrix:\n")
             np.savetxt(f, conf_matrix, fmt='%d')
             f.write("\n")
+
+    def roc_report(self, fpr, tpr, roc_auc, model_name,
+                   output_dir='roc_repot') -> None:
+        os.makedirs(output_dir, exist_ok=True)
+        report_file = os.path.join(output_dir, f'roc_report_{model_name}.txt')
+        with open(report_file, 'w') as f:
+            f.write(f"Model: {model_name}\n\n")
+            for i, cls in enumerate(fpr):
+                f.write(f"Class {i} ROC Curve (area = {roc_auc[i]:.2f}):\n")
+                f.write(f"False Positive Rate: {fpr[i]}\n")
+                f.write(f"True Positive Rate : {tpr[i]}\n\n")
 
     def report(self, file_dict: dict[str, str], encoded_labels: list[int],
                output_dir='classification_reports') -> None:
@@ -40,8 +54,13 @@ class ReportManager:
         accuracy_ann, conf_matrix_ann, model_name_ann = self.evaluation_metrics(
             testing_labels_ann, predictions_ann, model_name_ann)
 
-        self.write_report(report_ann, accuracy_ann,
-                          conf_matrix_ann, model_name_ann)
+        fpr_ann, tpr_ann, roc_auc_ann = self.roc_curve(testing_labels_ann,
+                                                       predictions_ann, model_name_ann)
+
+        self.classification_report(report_ann, accuracy_ann,
+                                   conf_matrix_ann, model_name_ann)
+
+        self.roc_report(fpr_ann, tpr_ann, roc_auc_ann, model_name_ann)
 
         # KNN model
         report_knn, testing_labels_knn, predictions_knn, model_name_knn = self.knn(
@@ -50,8 +69,13 @@ class ReportManager:
         accuracy_knn, conf_matrix_knn, model_name_knn = self.evaluation_metrics(
             testing_labels_knn, predictions_knn, model_name_knn)
 
-        self.write_report(report_knn, accuracy_knn,
-                          conf_matrix_knn, model_name_knn)
+        fpr_knn, tpr_knn, roc_auc_knn = self.roc_curve(testing_labels_knn,
+                                                       predictions_knn, model_name_knn)
+
+        self.classification_report(report_knn, accuracy_knn,
+                                   conf_matrix_knn, model_name_knn)
+
+        self.roc_report(fpr_knn, tpr_knn, roc_auc_knn, model_name_knn)
 
         # Naive Bayes model
         report_nb, testing_labels_nb, predictions_nb, model_name_nb = self.naives_bayes(
@@ -60,8 +84,13 @@ class ReportManager:
         accuracy_nb, conf_matrix_nb, model_name_nb = self.evaluation_metrics(
             testing_labels_nb, predictions_nb, model_name_nb)
 
-        self.write_report(report_nb, accuracy_nb,
-                          conf_matrix_nb, model_name_nb)
+        fpr_nb, tpr_nb, roc_auc_nb = self.roc_curve(
+            testing_labels_nb, predictions_nb, model_name_nb)
+
+        self.classification_report(report_nb, accuracy_nb,
+                                   conf_matrix_nb, model_name_nb)
+
+        self.roc_report(fpr_nb, tpr_nb, roc_auc_nb, model_name_nb)
 
         # SVM model
         report_svm, testing_labels_svm, predictions_svm, model_name_svm = self.support_vector_machine(
@@ -70,8 +99,13 @@ class ReportManager:
         accuracy_svm, conf_matrix_svm, model_name_svm = self.evaluation_metrics(
             testing_labels_svm, predictions_svm, model_name_svm)
 
-        self.write_report(report_svm, accuracy_svm,
-                          conf_matrix_svm, model_name_svm)
+        fpr_svm, tpr_svm, roc_auc_svm = self.roc_curve(testing_labels_svm,
+                                                       predictions_svm, model_name_svm)
+
+        self.classification_report(report_svm, accuracy_svm,
+                                   conf_matrix_svm, model_name_svm)
+
+        self.roc_report(fpr_svm, tpr_svm, roc_auc_svm, model_name_svm)
 
         # Decision Tree model
         report_dt, testing_labels_dt, predictions_dt, model_name_dt = self.decision_tree(
@@ -80,9 +114,17 @@ class ReportManager:
         accuracy_dt, conf_matrix_dt, model_name_dt = self.evaluation_metrics(
             testing_labels_dt, predictions_dt, model_name_dt)
 
-        self.write_report(report_dt, accuracy_dt,
-                          conf_matrix_dt, model_name_dt)
+        fpr_dt, tpr_dt, roc_auc_dt = self.roc_curve(
+            testing_labels_dt, predictions_dt, model_name_dt)
 
-        print("Report generated successfully 🗂️ 🗂️\n")
-        print("Check the classification_reports folder for the report")
-        print("Check the confusion_matrices_plt folder for the confusion matrices plots\n")
+        self.classification_report(report_dt, accuracy_dt,
+                                   conf_matrix_dt, model_name_dt)
+
+        self.roc_report(fpr_dt, tpr_dt, roc_auc_dt, model_name_dt)
+
+        print("✅ Report generated successfully 🗂️ 🗂️\n")
+        print("1. Check the classification_reports folder for the report\n")
+        print("2. Check the roc_curve_plt folder for the ROC curve plots\n")
+        print(
+            "3. Check the confusion_matrices_plt folder for the confusion matrices plots\n")
+        print("The folder can be found in the root directory of the project\n")
